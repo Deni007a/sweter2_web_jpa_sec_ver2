8ace591a36a5e9d8e933823bc0808823dc33c924
Deni007a <Deni007a@yandex.ru>
2018-10-01 19:06:28 +0300

    upgate properties config

--- src/main/java/sweter2_web_jpa_sec/sweter2_web_jpa_sec/Sweter2WebJpaSecApplication.java
+++ src/main/java/sweter2_web_jpa_sec/sweter2_web_jpa_sec/Sweter2WebJpaSecApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Sweter2WebJpaSecApplication {
 
     public static void main(String[] args) {
-        System.out.println("Start");
+
 
         SpringApplication.run(Sweter2WebJpaSecApplication.class, args);
 
